4ae7c3761fa9f707039e947d798e989f98d71bea
Anna <annkrv80@yandex.ru>
2022-10-01 01:03:21 +0700

    сортировка списка

---
+++ task005.py
@@ -0,0 +1,16 @@
+#Реализуйте алгоритм перемешивания списка.
+from random import randint
+import numbers
+import os
+from random import randrange
+def clear(): return os.system('cls')
+
+
+clear()
+list = [randint(-100, 100) for i in range(10)]
+print(list)
+for i in range(len(list) // 2):
+    box = list[i]
+    list[i] = list[-i-1]
+    list[-i-1] = box
+print(list)
