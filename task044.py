6648f83457f6a2d978543ba2f0f8284936e7a6d6
Anna <annkrv80@yandex.ru>
2022-10-09 21:10:16 +0700

    Найти произведение элементов списка

--- task044.py
+++ task044.py
@@ -1,10 +1,17 @@
 #Чтение файла
-f=open('file.txt','r',encoding='utf8')
-data=f.readlines()#['1\n', '-2']
-data = [int(i) for i in data] #замена строковых i на int i
+#f=open('file.txt','r',encoding='utf8')
+#data=f.readlines()#['1\n', '-2']
+#data = [int(i) for i in data] #замена строковых i на int i
 #data=f.read().split() #['1', '-2']
-print(data)
+#print(data)
 
-n = int (input('Введети число N '))
-sp = [i for i in range(-n,n+1)]
+n = int(input('Введети число N '))
+sp = [i for i in range(-n, n+1)]
 print(sp)
+with open('file.txt', 'r', encoding='utf') as f:
+    index = [int(i) for i in f.readlines()]
+print (index)
+mult = 1
+for ind in index:
+    mult*= sp[ind]
+print(mult)
