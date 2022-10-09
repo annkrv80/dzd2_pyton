a05baeaa14773eb28b3a94b9b6bab0d6d7ff22bf
Anna <annkrv80@yandex.ru>
2022-10-09 20:21:46 +0700

    Найти произведение элементов из списка

--- task044.py
+++ task044.py
@@ -4,3 +4,7 @@ data=f.readlines()#['1\n', '-2']
 data = [int(i) for i in data] #замена строковых i на int i
 #data=f.read().split() #['1', '-2']
 print(data)
+
+n = int (input('Введети число N '))
+sp = [i for i in range(-n,n+1)]
+print(sp)
