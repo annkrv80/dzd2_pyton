d908204e2e5aaec8f14c4fbc9910096f2be695b3
Anna <annkrv80@yandex.ru>
2022-10-09 20:08:13 +0700

    Чтение файла

---
+++ task044.py
@@ -0,0 +1,6 @@
+#Чтение файла
+f=open('file.txt','r',encoding='utf8')
+data=f.readlines()#['1\n', '-2']
+data = [int(i) for i in data] #замена строковых i на int i
+#data=f.read().split() #['1', '-2']
+print(data)
